97e46b9e3fc25cf0120e0fb4779432d136c6aad3
Misbahudin <misbahudin.id@gmail.com>
2023-11-27 15:00:54 +0700

    feat: penjumlahan

--- penjumlahan/main.cpp
+++ penjumlahan/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 
 using namespace std;
+// tgl 27-nov-23
+int main() {
+    // int a = 5;      // initial value: 5
+    // int b = 3;      // initial value: 3
+    // int c = 2;      // initial value: 2
+    // int result;     // initial value undetermined
 
-int main () {
-    cout << "Assalamualaikum, Dunia!" << endl;
+    // a += b;
+    // result = a - c;
 
-    int x,y,z;
+    // cout << result;
 
-    x = 10;
-    y = 10;
+    int a = 5, b = 3;
+    auto d = a;         // auto yaitu akan otomatis mengetahui nilai nya itu type data nya apa
+    decltype(b) e = 6;      // deklarasi type dari variable b yaitu int, ke variable e, sehingga variable e memiliki type data integer
 
-    z = x + y;
-
-    cout << z;
+    cout << "nilai d = " << d << endl;
+    cout << "nilai e = " << e;
 }
