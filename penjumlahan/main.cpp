853ab54585089886c60c2120a3326655baa4dd4d
Misbahudin <misbahudin.id@gmail.com>
2023-12-11 10:07:57 +0700

    feat: penjumlahan

--- penjumlahan/main.cpp
+++ penjumlahan/main.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 // tgl 27-nov-23
 int main() {
-    // int a = 5;      // initial value: 5
-    // int b = 3;      // initial value: 3
-    // int c = 2;      // initial value: 2
-    // int result;     // initial value undetermined
+    int a = 5;      // initial value: 5
+    int b = 3;      // initial value: 3
+    int c = 2;      // initial value: 2
+    int result;     // initial value undetermined
 
-    // a += b;
-    // result = a - c;
+    a += b;
+    result = a - c;
 
-    // cout << result;
+    cout << "result = " << result << endl;
 
-    int a = 5, b = 3;
-    auto d = a;         // auto yaitu akan otomatis mengetahui nilai nya itu type data nya apa
-    decltype(b) e = 6;      // deklarasi type dari variable b yaitu int, ke variable e, sehingga variable e memiliki type data integer
+    int d = 5, e = 3;
+    auto f = d;         // auto yaitu akan otomatis mengetahui nilai nya itu type data nya apa
+    decltype(f) g = 6;      // deklarasi type dari variable f yaitu int, ke variable g, sehingga variable g memiliki type data integer
 
-    cout << "nilai d = " << d << endl;
-    cout << "nilai e = " << e;
+    cout << "nilai f = " << f << endl;
+    cout << "nilai g = " << g;
 }
