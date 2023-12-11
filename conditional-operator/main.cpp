aeb85eab8d8ee63a35abc4673da19004a58e4fba
Misbahudin <misbahudin.id@gmail.com>
2023-12-11 10:07:04 +0700

    feat: conditional operator

---
+++ conditional-operator/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    a = 2;
+    b = 7;
+
+    c = (a > b) ? a : b;
+    cout << c << endl;
+
+    c = (a < b) ? a : b;
+    cout << c;
+
+}
