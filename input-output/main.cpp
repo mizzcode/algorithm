df5b72f845f50cc5f726a3c044a66e81187affc5
Misbahudin <misbahudin.id@gmail.com>
2023-12-11 10:07:29 +0700

    feat: input output

---
+++ input-output/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Masukan nilai: ";
+
+    cin >> a;
+
+    cout << "Nilai yang anda masukan adalah " << a << " Dan di Kali 2 adalah " << a * 2;
+}
